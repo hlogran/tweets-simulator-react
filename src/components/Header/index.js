a179251b74883024b1f4346ec06290643cc4f561
Hernan <hernanlogran@hotmail.com>
2020-01-05 16:22:23 -0300

    Refactor component: Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,1 +1,12 @@
-export { default } from "./Header.js";
+import React from "react";
+import TwitterLogo from "../../assets/twitter-logo.png";
+import "./styles.scss";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src={TwitterLogo} alt="Tweets Simulator" />
+      <h1>Tweets Simulator</h1>
+    </div>
+  );
+}
